515c7c27a3a8f93a6ffb8c70559c04e569b7cd6c
Nathan Connacher <nconnacher89@southhills.edu>
2021-09-09 09:55:12 -0400

    Repaired min command for ex1.html

--- js/nathan.js
+++ js/nathan.js
@@ -2,5 +2,22 @@
 // returns the min in the array
 function min(array) {
     console.log("stub for min");
-    return 35;
+
+    //Variable...
+    let x = 0;
+
+    //Pull min number from array...
+
+    for(i=0; i<array.length-1;i++)
+    {
+        if(i==0){
+            x=array[i];
+        }
+        else{
+            if(array[i]<x){
+                x=array[i];
+            }
+        }
+    }
+    return x;
 }
